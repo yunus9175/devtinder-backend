1c2bf231bbdc4d92071b386602cb2e24c9c62fa2
yunus9175 <yunus626@gmail.com>
2026-01-26 20:56:59 +0530

    different routes and regres in route

--- src/app.js
+++ src/app.js
@@ -8,22 +8,26 @@ const app = express();
 // res.send() is a method that sends a response to the client
 // IMPORTANT: Route order matters! Express matches routes TOP TO BOTTOM.
 // More specific routes should come BEFORE more general ones.
+
+// route "/ab?c" is equivalent to "/abc" or "/ab" or "/ac"
+// route "/ab+" is equivalent to "/ab" or "/abb" or "/abbb"
+// route "/ab*b" is equivalent to "/ab" or "/abb" or "/abbb" or "/abbbbb"
+// route "/ab{3}b" is equivalent to "/abbbb"
+// route "/ab{3,5}b" is equivalent to "/abbbb" or "/abbbbb"
+// route "/ab{3,}b" is equivalent to "/abbbb" or "/abbbbb" or "/abbbbbbb"
+// route "/ab{3,5}b" is equivalent to "/abbbb" or "/abbbbb"
+// route "/ab{3,5}b" is equivalent to "/abbbb" or "/abbbbb"
+// route "/a(bc)+d" is equivalent to "/abcbcd" or "/abcbcbcd" or "/abcbcbcbcd"
+// regex in route example: /user/[a-z0-9]+ ->
+//  this will match any route that starts with /user and is followed by one or more lowercase letters or numbers
+
 app.get("/", (req, res) => {
+    console.log(req.query);
+    console.log(req.params);
   res.send("<h1 style='color: pink;'>Hello World</h1>");
 });
 
-app.post("/user", (req, res) => {
-  res.send({
-    name: "John",
-    age: 20,
-    email: "john@example.com"
-  });
-});
-app.delete("/user", (req, res) => {
-  res.send({
-    message: "User deleted successfully"
-  });
-});
+
 
 // Start the server on port 3000
 app.listen(8080, () => {
